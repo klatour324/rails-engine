4f9592b53eb5e960ef34aea5e222d78c866d7565
Katy La Tour <klatour324@gmail.com>
2021-04-01 23:15:50 -0500

    Add relationship specs for Item model

---
+++ spec/models/items_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.desrcibe 'Item', type: :model do
+  describe 'relationships' do
+    it { should belong_to :merchant }
+    it { should have_many :invoice_items }
+    it { should have_many(:invoices).through(:invoice_items) }
+  end
+end
