d4e4f1940ccf0b1e153f65190fe9876dce9ce632
Katy La Tour <klatour324@gmail.com>
2021-04-03 22:07:35 -0500

    Add ItemSerializer with merchant_id and update unit_price attribute to float

---
+++ app/serializers/item_serializer.rb
@@ -0,0 +1,8 @@
+class ItemSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :description, :unit_price, :merchant_id
+
+  attribute :unit_price do |object|
+    object.unit_price.to_f
+  end
+end
