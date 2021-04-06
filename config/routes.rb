623dd375d7b40027926bb8dc23c9a35efa33783a
Katy La Tour <klatour324@gmail.com>
2021-04-06 01:20:01 -0500

    Update routes with merchnats/find_all

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get 'merchants/find_all', to: "merchants/search#index"
+
       resources :merchants, only: [:index, :show] do
         get '/items', to: "merchants/merchant_items#index"
       end
@@ -9,7 +11,6 @@ Rails.application.routes.draw do
       get '/items/find', to: "items/search#show"
 
       resources :items do
-        # resources :merchant, only: :show, controller: :item_merchant
         get '/merchant', to: "items/item_merchant#show"
       end
     end
