5b399865a0f797f2bd976aa3620a7a17e61a2bb6
Katy La Tour <klatour324@gmail.com>
2021-04-08 23:57:26 -0500

    Update AR method with ILKIE logic

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,18 +8,19 @@ class Item < ApplicationRecord
   validates_presence_of [:name, :description, :unit_price, :merchant_id], on: :create
 
   def self.find_item_by_name_fragment(searched_term)
-    where("lower(name) LIKE?", "%#{searched_term}%")
+    where("name ILIKE?", "%#{searched_term}%")
     .order(:name)
     .limit(1)
     .first
   end
 
-  # def self.find_item_by_name_fragment(searched_term)
-  #   where("lower(name) LIKE or lower(description) LIKE?", "%#{searched_term}%", "%#{searched_term}%")
-  #   .order(:name)
-  #   .limit(1)
-  #   .first
-  # end
+  def self.find_by_min_and_max_price(min_price, max_price)
+    where('unit_price <= ?', max_price )
+    .where('unit_price >= ?', min_price)
+    .order(:name)
+    .limit(1)
+    .first
+  end
 
   def self.items_most_revenue(quantity)
     select('items.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue')
